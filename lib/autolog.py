f5d9c0ca7a4aa016efc430993f10db47b59faeab
mh5 <devnull@localhost>
2005-12-08 13:47:49 +0000

    *** empty log message ***

--- lib/autolog.py
+++ lib/autolog.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import division
 
-__version__ = "$Revision: 1.4 $"
+__version__ = "$Revision: 1.5 $"
 
 import inspect
 import logging
@@ -20,11 +20,11 @@ class autolog(object):
     def __init__(self):
         self._name = self._get_name()
         self._logger = logging.getLogger(self._name)
-    
+
     def __getitem__(self, name):
         if name.startswith("."):
             name = self._name + name
-            
+
         return logging.getLogger(name)
 
     def __getattr__(self, name):
@@ -37,7 +37,7 @@ class autolog(object):
     @staticmethod
     def _get_name(stacklevel=2):
         res = inspect.stack()[stacklevel][0].f_globals["__name__"]
-        
+
         if res == "__main__":
             res = path(sys.argv[0]).namebase
 
@@ -45,3 +45,7 @@ class autolog(object):
             res = "root"
 
         return res
+
+    def die(self, msg, *args, **kwargs):
+        self.critical(msg, *args, **kwargs)
+        sys.exit(1)
