30e6dfc1405394fbb053200642c5ac0163466d3d
mmh1 <devnull@localhost>
2010-11-12 18:11:33 +0000

    change to setuptools

--- setup.py
+++ setup.py
@@ -7,13 +7,18 @@ Autolog makes the standard Python logging module easier to set up.
 
 __version__ = "0.1.2"
 
-from setuptools import setup
+# Copyright 2005,2007,2009-2010 Michael M. Hoffman <mmh1@washington.edu>
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import find_packages, setup
 
 doclines = __doc__.splitlines()
 name, short_description = doclines[0].split(": ")
 long_description = "\n".join(doclines[2:])
 
-url = "http://www.ebi.ac.uk/~hoffman/software/%s/" % name.lower()
+url = "http://noble.gs.washington.edu/~mmh1/software/%s/" % name.lower()
 download_url = "%s/%s-%s.tar.gz" % (url, name, __version__)
 
 classifiers = ["License :: OSI Approved :: GNU General Public License (GPL)",
@@ -24,7 +29,7 @@ setup(name=name,
       version=__version__,
       description=short_description,
       author="Michael Hoffman",
-      author_email="hoffman+%s@ebi.ac.uk" % name.lower(),
+      author_email="mmh1@uw.edu",
       url=url,
       download_url=download_url,
       license="GNU GPL v2",
@@ -32,5 +37,5 @@ setup(name=name,
       long_description = long_description,
       package_dir = {'': 'lib'},
       py_modules = ['autolog'],
-      install_requires=['path']
+      install_requires=['forked-path']
       )
