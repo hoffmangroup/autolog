9eb208ef5c25a4c65d9e6d8854f2f9b5ddecc3e1
mjmm13 <devnull@localhost>
2018-06-19 08:39:57 -0400

    Combined __future__ imports

--- lib/autolog.py
+++ lib/autolog.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-from __future__ import division
+from __future__ import absolute_import, division
 
-from __future__ import absolute_import
 __version__ = "$Revision: 1.5 $"
 
 import inspect
